d8a55bfcbcfe3609dd99b21e51ec298ace482d0b
Zach <zachary.thoene@okstate.edu>
2025-03-06 14:56:40 -0600

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Thoene, Zachary on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
